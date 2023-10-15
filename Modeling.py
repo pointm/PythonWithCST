e47f07860c0609508e2cdeeceb0dd85c5e388a43
pointm <2765211704@qq.com>
2023-10-15 13:32:37 +0800

    第一次调试，将调用CST的代码集成到Initial类里面

---
+++ Modeling.py
@@ -0,0 +1,312 @@
+import win32com.client
+import os
+
+
+def quotes_to_list(text):
+    lines = text.splitlines()
+    quoted_lines = ["" + line.lstrip() + "" for line in lines]
+    return quoted_lines
+
+
+class COMWithHistory():
+    mws = None
+
+    Classname = 'COMWithHistory'
+
+    def __init__(self, handle) -> None:
+        self.mws = handle
+        if self.mws == None:
+            raise ('MWS未进行初始化，请重新进行mws初始化')
+        else:
+            print(self.Classname+'MWS'+'初始化成功')
+            pass
+
+    def AddToHistoryWithList(self, mws, Tag, Command):
+        '''
+        添加到历史树
+        Command只能是队列哦，不能传输裸字符串！
+        适合在传递一小段一小段语句语句自定义程度较高的时候使用
+        '''
+        line_break = '\n'  # 这句话表示将每个单句用换行符链接起来
+        Command = line_break.join(Command)  # 这句话表示将每个单句用换行符链接起来
+        # AddToHistory ( string caption, string contents ) bool
+        # 隶属于ProjectObject的方法，必须使用mws对象进行调用
+        mws._FlagAsMethod("AddToHistory")
+        mws.AddToHistory(Tag, Command)
+
+    def AddToHistoryWithCommand(self, mws, Tag, Command):
+        '''
+        添加到历史树
+        可以传输字符串，适合整段整段懒得改的时候的语句使用
+        '''
+        # AddToHistory ( string caption, string contents ) bool
+        # 隶属于ProjectObject的方法，必须使用mws对象进行调用
+        mws._FlagAsMethod("AddToHistory")
+        mws.AddToHistory(Tag, Command)
+
+
+class Initial(COMWithHistory):
+    cst = None
+    mws = None
+
+    def __init__(self, lable='New', ProjectName=' ') -> None:
+        self.cst = self.OpenCST()
+        if lable == 'New':
+            handle = self.NewProject()
+            self.BackGroundInitial(mws=handle)
+            self.UnitInitial(mws=handle)
+            self.BoundaryInitial(mws=handle)
+        elif lable == 'Open':
+            handle = self.OpenProject(ProjectName=ProjectName)
+
+    def OpenCST(self):
+        # 调用COM对象，COM对象的名字叫CSTStudio.Application
+        self.cst = win32com.client.dynamic.Dispatch("CSTStudio.Application")
+        return self.cst
+
+    def NewProject(self):
+        # 新建一个微波工作室，返回一个操控当前项目的COM组件
+        self.cst.NewMWS()
+        self.mws = self.cst.Active3D()
+        return self.mws
+
+    def OpenProject(self, ProjectName):
+        # 打开一个项目，返回一个操控当前项目的COM组件
+        self.cst.OpenFile(ProjectName)
+        self.mws = self.cst.Active3D()
+        return self.mws
+
+    def BackGroundInitial(self, mws):
+        sCommand = ['With Background',
+                    '.ResetBackground',
+                    '.Type "PEC"',
+                    'End With']
+        self.AddToHistoryWithList(
+            mws=mws, Tag='Background Initial', Command=sCommand)
+
+    def UnitInitial(self, mws):
+        sCommand = ['With Units',
+                    '.Geometry "mm"',
+                    '.Frequency "ghz"',
+                    '.Time "ns"',
+                    'End With']
+        self.AddToHistoryWithList(mws, Tag='Unit Initial', Command=sCommand)
+
+    def BoundaryInitial(self, mws):
+        sCommand = ['With Boundary',
+                    '.Xmin "electric"',
+                    '.Xmax "electric"',
+                    '.Ymin "electric"',
+                    '.Ymax "electric"',
+                    '.Zmin "electric"',
+                    '.Zmax "electric"',
+                    '.Xsymmetry "none"',
+                    '.Ysymmetry "none"',
+                    '.Zsymmetry "none"',
+                    'End With']
+        self.AddToHistoryWithList(
+            mws, Tag='Boundary Initial', Command=sCommand)
+
+    def StoreParameter(self, mws, parametername, parametervalue, description):
+        sCommand = '''  
+        MakeSureParameterExists("%s", "%f")
+        SetParameterDescription  ( "%s", "%s" )
+    ''' % (parametername, parametervalue, parametername, description)
+        self.AddToHistoryWithCommand(
+            mws=mws, Tag='Store Parameter %s' % parametername, Command=sCommand)
+
+
+class Material(COMWithHistory):
+    def __init__(self, handle) -> None:
+        self.mws = handle
+        sCommand = '''With Material 
+     .Reset 
+     .Name "Sapphire"
+     .Folder ""
+     .Rho "0.0"
+     .ThermalType "Normal"
+     .ThermalConductivity "0"
+     .SpecificHeat "0", "J/K/kg"
+     .DynamicViscosity "0"
+     .Emissivity "0"
+     .MetabolicRate "0.0"
+     .VoxelConvection "0.0"
+     .BloodFlow "0"
+     .MechanicsType "Unused"
+     .IntrinsicCarrierDensity "0"
+     .FrqType "all"
+     .Type "Normal"
+     .MaterialUnit "Frequency", "GHz"
+     .MaterialUnit "Geometry", "mm"
+     .MaterialUnit "Time", "ns"
+     .MaterialUnit "Temperature", "Kelvin"
+     .Epsilon "9.4"
+     .Mu "1"
+     .Sigma "0"
+     .TanD "0.0"
+     .TanDFreq "0.0"
+     .TanDGiven "False"
+     .TanDModel "ConstTanD"
+     .SetConstTanDStrategyEps "AutomaticOrder"
+     .ConstTanDModelOrderEps "3"
+     .DjordjevicSarkarUpperFreqEps "0"
+     .SetElParametricConductivity "False"
+     .ReferenceCoordSystem "Global"
+     .CoordSystemType "Cartesian"
+     .SigmaM "0"
+     .TanDM "0.0"
+     .TanDMFreq "0.0"
+     .TanDMGiven "False"
+     .TanDMModel "ConstTanD"
+     .SetConstTanDStrategyMu "AutomaticOrder"
+     .ConstTanDModelOrderMu "3"
+     .DjordjevicSarkarUpperFreqMu "0"
+     .SetMagParametricConductivity "False"
+     .DispModelEps  "None"
+     .DispModelMu "None"
+     .DispersiveFittingSchemeEps "Nth Order"
+     .MaximalOrderNthModelFitEps "10"
+     .ErrorLimitNthModelFitEps "0.1"
+     .UseOnlyDataInSimFreqRangeNthModelEps "False"
+     .DispersiveFittingSchemeMu "Nth Order"
+     .MaximalOrderNthModelFitMu "10"
+     .ErrorLimitNthModelFitMu "0.1"
+     .UseOnlyDataInSimFreqRangeNthModelMu "False"
+     .UseGeneralDispersionEps "False"
+     .UseGeneralDispersionMu "False"
+     .NLAnisotropy "False"
+     .NLAStackingFactor "1"
+     .NLADirectionX "1"
+     .NLADirectionY "0"
+     .NLADirectionZ "0"
+     .Colour "0", "1", "0" 
+     .Wireframe "False" 
+     .Reflection "False" 
+     .Allowoutline "True" 
+     .Transparentoutline "False" 
+     .Transparency "0" 
+     .Create
+End With
+'''
+        self.AddToHistoryWithCommand(
+            mws=handle, Tag='addSapphire', Command=sCommand)
+
+
+class GeneralModel(COMWithHistory):
+    Component = ''
+    Name = ''
+    Material = ''
+
+    def __init__(self, handle) -> None:
+        self.mws = handle
+
+    def init(self):
+        pass
+
+    def create(self):
+        pass
+
+
+class Brick(GeneralModel):
+    Xrange = [0, 1]
+    Yrange = [0, 1]
+    Zrange = [0, 1]
+    Component = 'Hallo'
+    Name = 'World'
+    Material = 'PEC'
+    Classname = 'Brick'
+
+    def init(self, Component, Name, Material, Xrange, Yrange, Zrange):
+        self.Component = Component
+        self.Name = Name
+        self.Material = Material
+        self.Xrange = Xrange
+        self.Yrange = Yrange
+        self.Zrange = Zrange
+
+    def create(self, Tag):
+        Command = ['With Brick',
+                   '.Reset',
+                   '.Name "%s"' % self.Name,
+                   '.Component "%s"' % self.Component,
+                   '.Material "%s"' % self.Material,
+                   '.Xrange "%s", "%s"' % (self.Xrange[0], self.Xrange[1]),
+                   '.Yrange "%s", "%s"' % (self.Yrange[0], self.Yrange[1]),
+                   '.Zrange "%s", "%s"' % (self.Zrange[0], self.Zrange[1]),
+                   '.Create',
+                   'End With']
+        self.AddToHistoryWithList(self.mws, Tag, Command)
+
+
+class Cylinder(GeneralModel):
+    Material = "Vacuum"
+    Innerradius = 0
+    Outerradius = 0
+    Xcenter = 0
+    Ycenter = 0
+    Zcenter = 0
+    Xrange = [0, 0]
+    Yrange = [0, 0]
+    Zrange = [0, 0]
+    Range = [0, 0]
+    Segments = 0
+    Axis = 'z'
+
+    def init(self, Component, Name, Material, Axis, Innerradius, Outerradius, Xcenter, Ycenter, Zcenter, Range, Segments):
+        self.Component = Component
+        self.Name = Name
+        self.Material = Material
+        self.Innerradius = Innerradius
+        self.Outerradius = Outerradius
+        self.Xcenter = Xcenter
+        self.Ycenter = Ycenter
+        self.Zcenter = Zcenter
+        self.Range = Range
+        self.Segments = Segments
+        self.Axis = Axis
+
+    def create(self, Tag):
+        sCommand = ['With Cylinder',
+                    '.Reset',
+                    '.Name ("%s")' % self.Name,
+                    '.Component ("%s")' % self.Component,
+                    '.Material ("%s")' % self.Material,
+                    '.Axis ("%s")' % self.Axis,
+                    '.Outerradius ("%s")' % self.Outerradius,
+                    '.Innerradius ("%s")' % self.Innerradius,
+                    '.Xcenter (%f)' % self.Xcenter,
+                    '.Ycenter (%f)' % self.Xcenter,
+                    '.Zcenter (%f)' % self.Xcenter,]
+        if self.Axis == 'z':
+            sCommand = sCommand+['.Zrange ("%s", "%s")' % (self.Range[0], self.Range[1]),
+                                 '.Segments (%s)' % self.Segments,
+                                 '.Create',
+                                 'End With']
+        elif self.Axis == 'y':
+            sCommand = sCommand+['.Yrange ("%s", "%s")' % (self.Range[0], self.Range[1]),
+                                 '.Segments (%s)' % self.Segments,
+                                 '.Create',
+                                 'End With']
+        elif self.Axis == 'x':
+            sCommand = sCommand+['.Xrange ("%s", "%s")' % (self.Range[0], self.Range[1]),
+                                 '.Segments (%s)' % self.Segments,
+                                 '.Create',
+                                 'End With']
+        self.AddToHistoryWithList(mws=mws, Tag=Tag, Command=sCommand)
+
+
+def CstSaveAsProject(mws, projectName):
+    mws._FlagAsMethod("SaveAs")
+    mws.SaveAs(projectName, 'false')
+
+
+if __name__ == "__main__":
+    path = os.getcwd()  # 获取当前py文件所在文件夹路径，方便保存
+    filename = 'Test.cst'  # 保存的文件的名称，但是还没加后缀cst，后面保存的时候会自动加上后缀
+    projectName = os.path.join(path, filename)
+
+    init = Initial(lable='Open', ProjectName=projectName)
+    mws = init.mws
+    # CstSaveAsProject(mws, projectName)
+
+    SimulateFrequency = [6, 7]
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PythonWithCST
-This is a repository for modeling using Python to manipulate the CST. 这是一个用Python操纵CST进行建模的库。
+This is a repository for modeling using Python to manipulate the CST. 
+
+这是一个用Python操纵CST进行建模的库。（没错都是机翻）
+
+本仓库的灵感来源于[这个仓库](https://github.com/kaankvrck/Cst-Py-Api)，这个[B站教程](https://www.bilibili.com/video/BV1a7411K72H/?share_source=copy_web&vd_source=2c9beb25af00b14851dca086bf631efd)以及[这个CSDN教程](https://blog.csdn.net/weixin_52556029/article/details/126983128)
+
+他们的代码都很有启发性，但是CSDN教程只能在PYTHON老版本（具体来说，是python的3.6到3.9版本）才能运行，B站的教程能在高版本python运行，可以实现参数化建模，但是在进行扫参等操作的时候还是需要借助上面所提到的GitHub库里面提到的COM控件的方法进行操作。
+
+This repository was inspired by [ this repository ](https://github.com/kaankvrck/Cst-Py-Api) , this [bilibili tutorial](https://www.bilibili.com/video/BV1a7411K72H/?share_source=copy_web&vd_source=2c9beb25af00b14851dca086bf631efd) , and [ this CSDN tutorial ](https://blog.CSDN.net/weixin_52556029/article/details/126983128) 
+
+Their code is very enlightening, but CSDN tutorials can only run in older versions of Python (specifically, Python 3.6 to 3.9) , and bilibili tutorials can run in higher versions of Python, parameterized modeling can be achieved, but it is necessary to use the above-mentioned GitHub library in order to scan parameters and other operations of the COM control method.
